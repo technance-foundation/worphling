9c658873b323884ba1689c5e73d2dc1df9565ba2
Mohammad Ali Shahbazi <mohammadali.shahbazi@gmail.com>
2024-11-23 23:31:32 +0330

    Make playground load the config file

---
+++ packages/playground/worphling.config.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "api-key",
+    service: "openai",
+    sourceFile: "./locales/en.json",
+    outputDir: "./locales",
+    languages: ["es", "fr", "de"],
+    sourceLanguage: "en",
+};
