16c7244b919fcdb41f93b29edb8c307d131f0d62
Mohammad Ali Shahbazi <mohammadali.shahbazi@gmail.com>
2024-11-24 17:16:21 +0330

    Pass in source/targets to analyzer and get result back

--- packages/worphling/src/app/main.ts
+++ packages/worphling/src/app/main.ts
@@ -1,6 +1,9 @@
+import { omit } from "lodash-es";
+import { Analyzer } from "../Analyzer";
 import { JsonReader } from "../JsonReader";
 import { ConfigLoader } from "./ConfigLoader";
 import { handleErrors } from "./handleErrors";
+import { ANSI_COLORS } from "../constants";
 
 export async function main() {
     const configLoader = new ConfigLoader();
@@ -9,7 +12,14 @@ export async function main() {
         await configLoader.load();
         const config = configLoader.getConfig();
         const data = JsonReader.readAll(config.source.directory);
-        console.log(data);
+        const sourceKey = JsonReader.extractLanguageKey(config.source.file);
+        const targets = omit(data, sourceKey);
+        const result = Analyzer.compare(data[sourceKey], targets);
+
+        if (Object.entries(result).length === 0) {
+            console.log(ANSI_COLORS.green, "All target languages are already translated.");
+            process.exit(0);
+        }
     } catch (error) {
         handleErrors(error);
         process.exit(1);
